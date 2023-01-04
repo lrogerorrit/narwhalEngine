b6aa18865749c9c5c03ce9856485ea836d2f673c
lrogerorrit <49691021+lrogerorrit@users.noreply.github.com>
2023-01-04 15:39:17 +0100

    changed background color

--- src/narwhal_renderer.cpp
+++ src/narwhal_renderer.cpp
@@ -131,7 +131,7 @@ namespace narwhal {
 
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { 0.1f,0.1f,0.1f, 1.0f };
+		clearValues[0].color = { 0.01f,0.01f,0.01f, 1.0f }; // Background color
 		clearValues[1].depthStencil = { 1.0f, 0 };
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
 		renderPassInfo.pClearValues = clearValues.data();
