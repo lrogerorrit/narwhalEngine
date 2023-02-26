8a6e87993d57106640c9e92be13074109af0c983
lrogerorrit <49691021+lrogerorrit@users.noreply.github.com>
2023-02-26 11:08:44 +0100

    changed vulkan api version to 1.3

--- src/narwhal_device.cpp
+++ src/narwhal_device.cpp
@@ -83,11 +83,11 @@ namespace narwhal {
 
 		VkApplicationInfo appInfo = {};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "NarwhalEngine App";
+		appInfo.pApplicationName = "Black Hole App";
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.pEngineName = "No Engine";
+		appInfo.pEngineName = "Narwhal Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_3;
 
 		VkInstanceCreateInfo createInfo = {};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
